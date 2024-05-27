8099cc844699d7f849ceed25c3f580f45341426a
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-05-28 01:11:11 +0200

    Center cursor when hopping through files

--- neovim/lua/_/keymaps.lua
+++ neovim/lua/_/keymaps.lua
@@ -1,4 +1,10 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 vim.keymap.set('n', '-', ':Explore<CR>', { silent = true })
 
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-f>', '<C-f>zz')
+vim.keymap.set('n', '<C-b>', '<C-b>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
