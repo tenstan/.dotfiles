ccab27f74efd56b40c4d9de8dc5c986f4260ed51
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-05-31 18:05:44 +0200

    Switch to kangawa colorscheme

--- neovim/lua/_/plugins/colorscheme.lua
+++ neovim/lua/_/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
     {
-        'olimorris/onedarkpro.nvim',
+        'rebelot/kanagawa.nvim',
+        lazy = false,
         priority = 1000, -- Ensure it loads first
         config = function()
-            vim.cmd.colorscheme('onedark_dark')
+            vim.cmd('colorscheme kanagawa-dragon')
             vim.cmd('hi ColorColumn guibg=#101010')
         end
-    }
+    },
 }
