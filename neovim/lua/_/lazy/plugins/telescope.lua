35bfc0ba886e7912908f40462480c2b2c595e754
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-05-31 14:03:24 +0200

    Add telescope-fzf-native plugin for improved search

--- neovim/lua/_/lazy/plugins/telescope.lua
+++ neovim/lua/_/lazy/plugins/telescope.lua
@@ -3,13 +3,21 @@ return {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.7',
         dependencies = {
-            'nvim-lua/plenary.nvim'
+            'nvim-lua/plenary.nvim',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = 'make'
+            }
         },
-        config = function() 
-            require('telescope').setup({})
+        config = function()
+            local ts = require('telescope')
+
+            ts.setup({});
 
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+            ts.load_extension('fzf')
         end
     }
 }
