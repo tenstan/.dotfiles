a21a88531577ca5cbfef07bcb5af4dea0a9378aa
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-05-31 18:09:00 +0200

    Also enable autcomplete for "?"

--- neovim/lua/_/plugins/cmp.lua
+++ neovim/lua/_/plugins/cmp.lua
@@ -48,6 +48,13 @@ return {
             }
         })
 
+        cmp.setup.cmdline('?', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' }
+            }
+        })
+
         cmp.setup.cmdline(':', {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({
