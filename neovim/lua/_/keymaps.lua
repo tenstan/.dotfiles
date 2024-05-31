bf24973a583db86df92255a0bfc4fa5c400b3f7c
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-05-31 18:33:02 +0200

    Add mappings for inserting new line without entering insert mode

--- neovim/lua/_/keymaps.lua
+++ neovim/lua/_/keymaps.lua
@@ -2,9 +2,14 @@ vim.g.mapleader = ' '
 
 vim.keymap.set('n', '-', ':Explore<CR>', { silent = true })
 
+-- Keep cursor on the center of the screen when hopping
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<C-f>', '<C-f>zz')
 vim.keymap.set('n', '<C-b>', '<C-b>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- For inserting empty lines without entering insert mode
+vim.keymap.set('n', '<leader>o', 'o<Esc>k')
+vim.keymap.set('n', '<leader>O', 'O<Esc>j')
