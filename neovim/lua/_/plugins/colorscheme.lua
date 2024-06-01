8536af388d091ca73f22d1aab1a1a8e75a977d3b
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-06-01 23:16:02 +0200

    Merge lualine with colorscheme.lua

--- neovim/lua/_/plugins/colorscheme.lua
+++ neovim/lua/_/plugins/colorscheme.lua
@@ -8,4 +8,16 @@ return {
             vim.cmd('hi ColorColumn guibg=#101010')
         end
     },
+    {
+        'nvim-lualine/lualine.nvim',
+        config = function()
+            require('lualine').setup({
+                options = {
+                    icons_enabled = false,
+                    section_separators = '',
+                    component_separators = ''
+                }
+            })
+        end
+    }
 }
