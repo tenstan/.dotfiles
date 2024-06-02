475c9a548739ab825d3810eecabdbe16861996a1
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-06-02 13:34:20 +0200

    Migrate off of legacy opts for nvim-ts-autotag

--- neovim/lua/_/plugins/autocomplete.lua
+++ neovim/lua/_/plugins/autocomplete.lua
@@ -2,8 +2,10 @@ return {
     {
         'windwp/nvim-ts-autotag',
         opts = {
-            enable_close = true,
-            enable_rename = false, -- Handle through surround.nvim instead
+            opts = {
+                enable_close = true,
+                enable_rename = false, -- Handle through surround.nvim instead
+            }
         },
         config = true,
     },
