d8f1324baac7c309c08814364a6782c934c3db87
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-06-03 00:21:53 +0200

    Add extra mappings for finding symbols with telescope

--- neovim/lua/_/plugins/telescope.lua
+++ neovim/lua/_/plugins/telescope.lua
@@ -13,7 +13,13 @@ return {
             require('telescope').load_extension('fzf')
 
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>ff', function() builtin.find_files { hidden = true, } end)
+            vim.keymap.set('n', '<leader>fF', function() builtin.find_files { hidden = true, no_ignore = true } end)
+            vim.keymap.set('n', '<leader>fc', builtin.grep_string)
+            vim.keymap.set('n', '<leader>fw', builtin.live_grep)
+            vim.keymap.set('n', '<leader>fr', builtin.resume)
+            vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols)
+            vim.keymap.set('n', '<leader>fsw', builtin.lsp_dynamic_workspace_symbols)
         end
     }
 }
