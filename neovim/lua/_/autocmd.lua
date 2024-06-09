9b48cbedf62ad961c714bb0c76b0f771074c4f8c
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-06-09 21:04:01 +0200

    Highlight on yank in nvim

--- neovim/lua/_/autocmd.lua
+++ neovim/lua/_/autocmd.lua
@@ -1,8 +1,20 @@
 local my_group = vim.api.nvim_create_augroup('MyGroup', {})
+local yank_group = vim.api.nvim_create_augroup('HighlightYank', {})
 
 -- Remove trailing whitespace on write
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
     group = my_group,
     pattern = '*',
     command = [[%s/\s\+$//e]],
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function ()
+       vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 200
+        })
+    end
 })
