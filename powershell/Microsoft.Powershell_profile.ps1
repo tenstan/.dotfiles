04ed0b544cb5168f93365fd540b74eecb0bdf69c
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-06-11 20:49:47 +0200

    Use bash style autocomplete for powershell

--- powershell/Microsoft.Powershell_profile.ps1
+++ powershell/Microsoft.Powershell_profile.ps1
@@ -2,3 +2,6 @@ Import-Module posh-git
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/onehalf.minimal.omp.json" | Invoke-Expression
 $env:POSH_GIT_ENABLED = $true # Make OhMyPosh aware of posh-git
+
+Set-PSReadLineKeyHandler -Key Tab -Function Complete # Bash style autocomplete
+Set-PSReadLineOption -BellStyle None
