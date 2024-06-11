38773284513781f4c0fa8320ebe8180eac2ca582
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-06-11 22:12:23 +0200

    Add mapping for pasting inline

--- neovim/lua/_/keymaps.lua
+++ neovim/lua/_/keymaps.lua
@@ -15,7 +15,10 @@ vim.keymap.set('n', '<leader>o', 'o<Esc>k')
 vim.keymap.set('n', '<leader>O', 'O<Esc>j')
 
 -- Copy to system clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({'n', 'v'}, '<leader>y', [["+y]])
 
 -- Delete to black hole register
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({'n', 'v'}, '<leader>d', [["_d]])
+
+-- Paste inline
+vim.keymap.set('n', '<leader>p', 'pkJ')
