fa04c99b97153e3a410ed142161a4021e89620f8
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-06-23 18:53:21 +0200

    Alias bat with cat in Powershell

--- powershell/Microsoft.Powershell_profile.ps1
+++ powershell/Microsoft.Powershell_profile.ps1
@@ -1,10 +1,19 @@
 Import-Module posh-git
 
+# Set up theme
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/onehalf.minimal.omp.json" | Invoke-Expression
 $env:POSH_GIT_ENABLED = $true # Make OhMyPosh aware of posh-git
 
+# Set up autocomplete
 Set-PSReadLineKeyHandler -Key Tab -Function Complete # Bash style autocomplete
 Set-PSReadLineOption -BellStyle None
 
+# Utility shortcuts
+$env:BAT_PAGING = 'never' # Disable paging with bat
 
+Remove-Item Alias:cat -Force
+
+Set-Alias -Name cat -Value bat -Force
+
+# Path shortcuts
 function df { Set-Location -Path "$home\.dotfiles" }
