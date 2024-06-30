99f6ca23429946b0f83932ac03103227649209f6
ovalice <31350208+ovalice@users.noreply.github.com>
2024-06-30 21:59:48 +0200

    Rename symbol search keymaps

--- neovim/lua/_/plugins/fzf-lua.lua
+++ neovim/lua/_/plugins/fzf-lua.lua
@@ -18,8 +18,8 @@ return {
             vim.keymap.set('n', '<leader>fc', function() fzf.grep_cword() end)
             vim.keymap.set('n', '<leader>fw', function() fzf.live_grep_native() end)
             vim.keymap.set('n', '<leader>fr', function() fzf.resume() end)
-            vim.keymap.set('n', '<leader>fs', function() fzf.lsp_document_symbols() end)
-            vim.keymap.set('n', '<leader>fsw', function() fzf.lsp_live_workspace_symbols() end)
+            vim.keymap.set('n', '<leader>fds', function() fzf.lsp_document_symbols() end)
+            vim.keymap.set('n', '<leader>fs', function() fzf.lsp_live_workspace_symbols() end)
         end
     }
 }
