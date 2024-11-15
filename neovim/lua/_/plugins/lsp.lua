d0bde23dd0a831f38320fb73bc0bd6b6b488b2d7
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-11-15 12:13:45 +0100

    Set up LSP keymaps through Telescope, cause the default of outputting to a buffer sucks

--- neovim/lua/_/plugins/lsp.lua
+++ neovim/lua/_/plugins/lsp.lua
@@ -5,15 +5,16 @@
 local on_lsp_attach = function(client, bufnr)
     local opts = { buffer = bufnr }
 
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<C-h>', vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+
+    -- Telescope mappings:
+    -- gd - go to definition
+    -- gt - go to type definition
+    -- gi - go to implementaiton
+    -- gr - go to references
 end
 
 return {
--- neovim/lua/_/plugins/telescope.lua
+++ neovim/lua/_/plugins/telescope.lua
@@ -1,5 +1,4 @@
 return {
-
     'nvim-telescope/telescope.nvim',
     tag = '0.1.7',
     dependencies = {
@@ -22,12 +21,52 @@ return {
 
         local builtin = require('telescope.builtin')
 
-        vim.keymap.set('n', '<leader>ff', builtin.find_files)
-        vim.keymap.set('n', '<leader>fF', function() builtin.find_files { hidden = true, no_ignore = true } end)
-        vim.keymap.set('n', '<leader>fc', builtin.grep_string)
-        vim.keymap.set('n', '<leader>fw', builtin.live_grep)
-        vim.keymap.set('n', '<leader>fr', builtin.resume)
-        vim.keymap.set('n', '<leader>fds', builtin.lsp_document_symbols)
+        local file_ignore_patterns = {
+            "node_modules/",
+            "dist/",
+            "package%-lock%.json",
+            "yarn%.lock",
+            "%.git/"
+        }
+
+        vim.keymap.set('n', 'gd', builtin.lsp_definitions)
+        vim.keymap.set('n', 'gt', builtin.lsp_type_definitions)
+        vim.keymap.set('n', 'gi', builtin.lsp_implementations)
+        vim.keymap.set('n', 'gr', builtin.lsp_references)
         vim.keymap.set('n', '<leader>fs', builtin.lsp_dynamic_workspace_symbols)
+
+        vim.keymap.set('n', '<leader>xd', builtin.diagnostics)
+        vim.keymap.set('n', '<leader>xq', builtin.quickfix)
+
+        vim.keymap.set('n', '<leader>fr', builtin.resume)
+
+        vim.keymap.set('n', 'ff', function()
+            builtin.find_files {
+                file_ignore_patterns = file_ignore_patterns,
+                hidden = true,
+                no_ignore = true
+            }
+        end)
+        vim.keymap.set('n', '<leader>fF', function()
+            builtin.find_files {
+                hidden = true,
+                no_ignore = true
+            }
+        end)
+        vim.keymap.set('n', '<leader>fw', function ()
+            builtin.live_grep {
+                file_ignore_patterns = file_ignore_patterns,
+                additional_args = function()
+                    return { '--hidden' }
+                end
+            }
+        end)
+        vim.keymap.set('n', '<leader>fW', function()
+            builtin.live_grep {
+                additional_args = function ()
+                   return { '--hidden', '--no-ignore' }
+                end
+            }
+        end)
     end
 }
