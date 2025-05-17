1ef3f189d782fd9be29162f23a8316a6f5534e2f
Stan Martens <31350208+tenstan@users.noreply.github.com>
2025-05-18 01:21:16 +0200

    Configure neovim code snippets

--- neovim/lua/_/plugins/autocomplete.lua
+++ neovim/lua/_/plugins/autocomplete.lua
@@ -18,6 +18,16 @@ return {
         }
     },
     {
+        'L3MON4D3/LuaSnip',
+        dependencies = {
+            'rafamadriz/friendly-snippets'
+        },
+        build = 'make install_jsregexp',
+        config = function()
+            require('luasnip.loaders.from_vscode').lazy_load()
+        end
+    },
+    {
         'hrsh7th/nvim-cmp',
         event = { 'InsertEnter', 'CmdlineEnter' },
         dependencies = {
